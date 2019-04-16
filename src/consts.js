76dcee84d94603f20c941ae0de6865fd2971cb8d
renato.vicente <rntvicente@gmail.com>
2019-04-16 11:57:17 -0300

    alteração de const

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
-  API_URL: 'https://backend-my-money.herokuapp.com/api',
-  OAPI_URL: 'https://backend-my-money.herokuapp.com/oapi'
+  API_URL: process.env.API_URL ? process.env.API_URL : 'http://localhost:3000/api',
+  OAPI_URL: process.env.OAPI_URL ? process.env.OAPI_URL : 'http://localhost:3000/oapi'
 };
